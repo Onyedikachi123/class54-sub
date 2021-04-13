34dc40ee21888b365bdd26de3d1337dacc938cae
Kachi <onyedikachierugo@gmail.com>
2021-04-13 02:11:18 +0100

    subscribtion-page done

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="footer">
+  <v-container>
+      <p class="text-center">Copyright  © 2021. Class54 Education LTD.</p>
+  </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.footer  {
+    margin-top: 300px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,28 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/Subscription',
+    name: 'Subscription',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Subscription.vue')
+  },
+  {
+    path: '/Download-page',
+    name: 'Download-page',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Download-page.vue')
+  },
+  {
+    path: '/Activate-page',
+    name: 'Activate-page',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Activate-page.vue')
   }
 ]
 
